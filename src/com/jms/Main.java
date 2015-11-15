b03225bffc56cd610c14367cac30f7d67768cc65
Jakub Szlendak <jakub.szlendak@logicit.eu>
2015-11-15 21:49:54 +0100

    GUI mockup working

--- src/com/jms/Main.java
+++ src/com/jms/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+
+        Runnable task = MainWindow::makeGUI;
+        javax.swing.SwingUtilities.invokeLater(task);
     }
 }
